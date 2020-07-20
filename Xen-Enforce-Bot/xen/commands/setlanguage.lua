1e2b7d089129332b35a3a20b305f2383c115237e
XAYRGA <42713335+XAYRGA@users.noreply.github.com>
2020-07-20 17:16:54 -0400

    Fix case that would crash the bot

--- Xen-Enforce-Bot/xen/commands/setlanguage.lua
+++ Xen-Enforce-Bot/xen/commands/setlanguage.lua
@@ -8,7 +8,7 @@ function COMMAND:Execute(gc,msg,VFD,doubt, args)
 	end 
 	args[1] = string.lower(args[1])
 	if not Localization.supports(args[1])  then 
-		msg:replySendMessage(Localization.getStringLocalized(gc:getString("language"), "config/languageNotSupported"),args[1]) 
+		msg:replySendMessage(Localization.getStringLocalized(gc:getString("language"), "config/languageNotSupported",args[1])) 
 		return 
 	end 
 
