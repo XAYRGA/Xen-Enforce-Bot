c46321cae7b18c704fecbf37ed7a0d0b8a8808b4
XAYRGA <dzbush25@gmail.com>
2020-10-01 18:46:01 -0400

    update schema, add config example

--- Xen-Enforce-Bot/xenfbot4schema.sql
+++ Xen-Enforce-Bot/xenfbot4schema.sql
@@ -1,3 +1,16 @@
+-- --------------------------------------------------------
+-- Host:                         sea.xayr.ga
+-- Server version:               10.1.44-MariaDB-0ubuntu0.18.04.1 - Ubuntu 18.04
+-- Server OS:                    debian-linux-gnu
+-- HeidiSQL Version:             11.0.0.5919
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
 
 -- Dumping database structure for xenf_test
 CREATE DATABASE IF NOT EXISTS `xenf_test` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
@@ -21,14 +34,14 @@ CREATE TABLE IF NOT EXISTS `cleanup` (
   `when` int(11) NOT NULL,
   `life` int(11) NOT NULL,
   PRIMARY KEY (`uid`)
-) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=53682 DEFAULT CHARSET=utf8mb4;
 
 -- Data exporting was unselected.
 
 -- Dumping structure for table xenf_test.configs
 CREATE TABLE IF NOT EXISTS `configs` (
   `groupid` bigint(20) NOT NULL,
-  `language` text NOT NULL,
+  `language` text CHARACTER SET utf8 NOT NULL,
   `attackmode` tinyint(1) NOT NULL DEFAULT '0',
   `smartdetect` tinyint(1) NOT NULL DEFAULT '0',
   `phraseban` tinyint(1) NOT NULL DEFAULT '0',
@@ -40,12 +53,13 @@ CREATE TABLE IF NOT EXISTS `configs` (
   `verifyannounce` tinyint(1) NOT NULL DEFAULT '1',
   `verifymute` tinyint(1) NOT NULL DEFAULT '0',
   `verifydelay` int(11) NOT NULL DEFAULT '30',
-  `verifyask` text NOT NULL,
-  `verifymessage` text NOT NULL,
+  `verifyask` mediumtext CHARACTER SET utf16 COLLATE utf16_unicode_ci NOT NULL,
+  `verifymessage` text CHARACTER SET utf8 NOT NULL,
   `mediadelay` tinyint(1) DEFAULT '0',
   `mediadelaytime` int(11) DEFAULT '30',
+  `dontdeletejoinmessage` tinyint(1) DEFAULT '0',
   PRIMARY KEY (`groupid`) USING BTREE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf32;
 
 -- Data exporting was unselected.
 
@@ -57,7 +71,7 @@ CREATE TABLE IF NOT EXISTS `removals` (
   `cause` text,
   `when` bigint(20) DEFAULT NULL,
   PRIMARY KEY (`uid`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=25915 DEFAULT CHARSET=utf8mb4;
 
 -- Data exporting was unselected.
 
@@ -76,7 +90,7 @@ CREATE TABLE IF NOT EXISTS `verify` (
   `message` bigint(20) DEFAULT '0',
   `joinmessage` bigint(20) DEFAULT '0',
   PRIMARY KEY (`uid`)
-) ENGINE=InnoDB AUTO_INCREMENT=34 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=47316 DEFAULT CHARSET=utf8mb4;
 
 -- Data exporting was unselected.
 
@@ -90,3 +104,6 @@ CREATE TABLE IF NOT EXISTS `verify_doubt` (
 
 -- Data exporting was unselected.
 
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
